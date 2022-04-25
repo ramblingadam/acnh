a69a7549dc503ab21c82266870f5e8dac4738d19
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-04-25 02:45:54 -0500

    category switching function. start fish

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
-//------ INITIALIZATION ---------
+// ! INITIALIZATION ---------
 // Declare villager data storage
 let allVillagers
+let allFish
+let allSea
+let allBugs
+
+let currentCategory
 
 // Turn on music by default.
 musicOn = true
 
-// Grab current date to check birthdays
+// Store current date to check birthdays
 let now = new Date()
 
-// GRAB HTML ELEMENTS
+// ! GRAB HTML ELEMENTS
 // Grab audio element for BG Music
 const bgAudio = document.querySelector('#bgMusic')
-// Grab mute button
+
+// Grab music toggle button
 const musicToggle = document.querySelector('#musicToggle')
+
+
+
+
+// Grab main menu items
+const btnList = document.querySelectorAll('nav ul li')
+const btnVillagers = document.querySelector('#btnVillagers')
+const btnFish = document.querySelector('#btnFish')
+// const btnSea = document.querySelector('#btnSea')
+// const btnFossils = document.querySelector('#btnFossils')
+// const btnArt = document.querySelector('#btnArt')
+// const btnSongs = document.querySelector('#btnSongs')
+
 // Grab search bar
 const searchBar = document.querySelector('#search')
-// Grab main menu items
-const btnVillagers = document.querySelector('#btnVillagers')
+
 
 // Grab content grid
 const contentGrid = document.querySelector('#contentGrid')
@@ -26,23 +44,25 @@ const contentGrid = document.querySelector('#contentGrid')
 
 
 
-// EVENT LISTENERS
+// ! EVENT LISTENERS
 // Music Toggle
 musicToggle.addEventListener('click', toggleMusic)
 
-// Main Menu - Villagers Button
-
+// Main Menu Buttons
+btnVillagers.addEventListener('click', () => {displayVillagers()})
+btnFish.addEventListener('click', () => {displayFish()})
 
 
 // Active search
 searchBar.addEventListener('input', search)
 
+// ! RUN INITIAL FUNCTIONS
 // Start by loading villagers by default.
 getVillagers()
 // Play music.
 setTimeout(musicSelection, 1000)
 
-
+// ! MUSIC TOGGLE FUNCITON
 function toggleMusic() {
   if(musicOn) {
     musicOn = false
@@ -57,7 +77,7 @@ function toggleMusic() {
   }
 }
 
-
+// ! ACTIVE SEARCH FUNCTION
 function search(e) {
   clearGrid()
   searchString = e.target.value.toLowerCase()
@@ -71,7 +91,7 @@ function search(e) {
 }
 
 
-
+// ! HOURLY MUSIC SELECTION
 function musicSelection() {
   let weather = 'Sunny'
   let hour = String(now.getHours())
@@ -87,7 +107,7 @@ function musicSelection() {
       const musicURI = musicData[`BGM_24Hour_${hour}_${weather}`]['music_uri']
       console.log(musicURI)
       bgAudio.src = musicURI
-      bgAudio.play()
+      // bgAudio.play()
 
   })
   .catch(err => {
@@ -97,13 +117,24 @@ function musicSelection() {
 
 
 
-// CLEAR CONTENT GRID
+// ! CLEAR CONTENT GRID
 function clearGrid() {
   items = document.querySelectorAll('.contentItem')
   items.forEach(item => item.remove())
 }
 
-// GRAB VILLAGER DATA
+// ! UPDATE CURRENT CATEGORY
+function updateCategory(category) {
+  btnList.forEach(btn => {
+    if(btn.matches(`#btn${category}`)) {
+      btn.classList.add('currentCategory')
+      currentCategory = category
+    }
+    else btn.classList.remove('currentCategory')
+  })
+}
+
+// ! GRAB VILLAGER DATA
 function getVillagers() {
   fetch(`https://acnhapi.com/v1a/villagers/`)
   .then(res => res.json())
@@ -120,8 +151,14 @@ function getVillagers() {
 })
 }
 
-// DISPLAY VILLAGERS
-function displayVillagers(villagerArray) {
+
+
+// ! DISPLAY VILLAGERS
+function displayVillagers(villagerArray = allVillagers) {
+  console.log(villagerArray)
+
+  updateCategory('Villagers')
+  clearGrid()
   villagerArray.forEach(villager => {
     const li = document.createElement('li')
     // console.log(li)
@@ -161,3 +198,62 @@ function displayVillagers(villagerArray) {
   })
 }
 
+// ! GRAB FISH DATA
+function getFish() {
+  fetch(`https://acnhapi.com/v1a/villagers/`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    allFish = data
+
+    // Run villager display function
+    displayVillagers(allFish)
+  
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+})
+}
+
+// ! DISPLAY FISH
+function displayFish(fishArray = allFish) {
+  updateCategory('Fish')
+  clearGrid()
+  villagerArray.forEach(villager => {
+    const li = document.createElement('li')
+    // console.log(li)
+  
+    // console.log(villager)
+    li.classList.add('contentItem')
+    li.classList.add('villager')   
+
+
+    // Check if the villager has a birthday this month. If so, add the birthdayMonth class via template literal.
+    // First create a birthday string JS can read.
+    const birthday = new Date(villager['birthday-string'].slice(0,-2) + ', ' + now.getFullYear())
+    // Init birthdayMonth string for template literal to be empty.
+    let birthdayMonth = ''
+    if(birthday.getMonth() === now.getMonth()) {
+      // console.log(`${villager.name['name-USen']}'s birthday is this month!`)
+      // birthdayMonth = ' birthdayMonth'
+      li.classList.add('birthdayMonth')
+      if(birthday.getDate() === now.getDate()) {
+        li.classList.add('birthdayDay')
+      }
+    }    
+
+    // Determine gender
+    let genderString
+    if(villager.gender === 'Male') genderString = '<i class="fa-solid fa-mars"></i>'
+    else genderString = '<i class="fa-solid fa-venus"></i>'
+
+    // CREATING VILLAGER TILES
+    li.innerHTML = `<h2 class="name">${villager.name['name-USen']}</h3><h4 class="personality">${genderString}${villager.personality}</h4><h4 class="birthday${birthdayMonth}"><i class="fa-solid fa-cake-candles"></i> ${villager['birthday-string']}</h4><div class="villagerImgBox"><img src="${villager['image_uri']}"><div class="catchphraseBox"><span class="catchphrase">"${villager['catch-phrase']}!"</span><img src="${villager['icon_uri']}"></div></div><p class="quote">${villager.saying}</p>`
+
+    
+
+    contentGrid.appendChild(li)
+    // console.log('-----')
+    // console.log(now.toLocaleString())
+  })
+}
